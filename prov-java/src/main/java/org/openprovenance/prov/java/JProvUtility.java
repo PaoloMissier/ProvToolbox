215b2d9ab9531872037bd67e21a8aef855f1c7c3
Paolo Missier <pmissier@acm.org>
2012-09-26 09:02:07 +0100

    adding proven modules
    William Martin's code

---
+++ prov-java/src/main/java/org/openprovenance/prov/java/JProvUtility.java
@@ -0,0 +1,33 @@
+package org.openprovenance.prov.java;
+
+import org.antlr.runtime.tree.CommonTree;
+import org.openprovenance.prov.java.component4.Bundle;
+import org.openprovenance.prov.notation.ASNConstructor;
+import org.openprovenance.prov.notation.TreeTraversal;
+import org.openprovenance.prov.notation.Utility;
+
+public class JProvUtility extends Utility {
+
+	public Bundle convertASNToJava(String file) throws java.io.IOException, Throwable { 
+		CommonTree tree = convertASNToTree(file);
+		Bundle b = convertTreeToJava(tree);
+		return b;
+	}
+	
+	public Bundle convertTreeToJava(CommonTree tree) {
+		Object o = new TreeTraversal(new JavaConstructor()).convert(tree);
+		return (Bundle) o;
+	}
+	
+	public String convertJavaToASN(Bundle b) {
+		JProvTraversal jt = new JProvTraversal(new JProvTreeConstructor(new ASNConstructor()));
+        Object o = jt.convert(b);
+        return (String) o;
+    }
+	
+	public Bundle convertJavaToJava(Bundle b) {
+		JProvTraversal jt = new JProvTraversal(new JProvTreeConstructor(new JavaConstructor()));
+		Object o = jt.convert(b);
+		return (Bundle) o;
+	}
+}
